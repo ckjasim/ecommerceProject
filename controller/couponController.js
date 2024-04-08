2795c7b6b0cc525d6657278c7b83a1526a0e516d
jasim <bck.jasim@gmail.com>
2024-04-08 16:21:26 +0530

    addCoupon

--- controller/couponController.js
+++ controller/couponController.js
@@ -1,31 +1,50 @@
+const couponSchema = require("../model/couponData");
+
 //------------------------------admin--------------------------
 
 const loadCoupon = async (req, res) => {
-    try {
+  try {
 
-    
-        res.render('coupons')
-        
-      
-        
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+const couponData=await couponSchema.find()
+
+    res.render("coupons",{couponData});
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 const loadNewCoupon = async (req, res) => {
-    try {
+  try {
+    res.render("addCoupon");
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+const addCoupon = async (req, res) => {
+  try {
+    const { code, name, description, percentage, minAmount, date } = req.body;
+    console.log("kkk111");
+    console.log(code)
+    console.log(name)
+    console.log(description)
+    const couponData = new couponSchema({
+      code: code.toUpperCase(),
+      name: name,
+      description: description,
+      percentage: percentage,
+      minAmount: minAmount,
+      expiredAt: date,
+     
+    });
+    await couponData.save();
+res.redirect('/newCoupon')
 
-    
-        res.render('addCoupon')
-        
-      
-        
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
-module.exports={
-    loadCoupon,
-    loadNewCoupon
-}
+module.exports = {
+  loadCoupon,
+  loadNewCoupon,
+  addCoupon,
+};
